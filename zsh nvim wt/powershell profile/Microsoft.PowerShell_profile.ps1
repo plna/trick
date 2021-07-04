89ad4c0cd09c176d0f845dea6b45bf43fe5493cb
Nhut Anh <44976168+plna@users.noreply.github.com>
2021-07-04 17:02:28 +0700

    Update Microsoft.PowerShell_profile.ps1

--- zsh nvim wt/powershell profile/Microsoft.PowerShell_profile.ps1
+++ zsh nvim wt/powershell profile/Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,6 @@
 # type   notepad $profile
 
 
-
-
 # Theme
 Set-PoshPrompt -Theme pure
 Import-Module Get-ChildItemColor
@@ -12,11 +10,7 @@ Import-Module Get-ChildItemColor
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-# Autocompletion for arrow keys
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Key "Alt+f" -Function ForwardWord
 Set-PSReadLineKeyHandler -Key "Alt+f" -Function ForwardWord
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptSuggestion
