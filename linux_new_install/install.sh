0ebe22bf72986f5ad3fe51b641f2c279903ae777
Nhut Anh (Peter) <44976168+plna@users.noreply.github.com>
2024-03-11 14:53:19 +0700

    Update install.sh

--- linux_new_install/install.sh
+++ linux_new_install/install.sh
@@ -13,9 +13,7 @@ sudo apt install ruby
 sudo apt install ruby-dev
 sudo gem install colorls
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+$ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+$ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+$ source ~/.zshrc
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-# .zshrc   plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
